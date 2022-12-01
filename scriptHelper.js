3e60ba6742179b68d7e3d93590837acd2211b134
LauraKnoblock <laura.e.knoblock@gmail.com>
2022-12-01 17:50:45 -0600

    Alerts working, not printing on html yet

--- scriptHelper.js
+++ scriptHelper.js
@@ -1,12 +1,13 @@
 // Write your helper functions here!
 require('isomorphic-fetch');
 
+
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
    // Here is the HTML formatting for our mission target div.
    /*
                 <h2>Mission Destination</h2>
                 <ol>
-                    <li>Name: </li>
+                    <li>Name: </li> 
                     <li>Diameter: </li>
                     <li>Star: ${star}</li>
                     <li>Distance from Earth: </li>
@@ -17,12 +18,42 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-   
-}
+    if (testInput === "") {
+        return "Empty";
+    } else if (isNaN(testInput)) {
+        return "Not a Number";
+    } else if (!isNaN(testInput)) {
+        return "Is a Number";
+    }
+ 
+    
+  }
+
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+    console.log('formSubmission')
+    let window = document.window;
+    let faultyItems = document.getElementById('faultyItems');
+    let pilotStatus = document.getElementById('pilotStatus');
+    let copilotStatus = document.getElementById('copilotStatus');
+    let fuelStatus = document.getElementById('fuelStatus');
+
+    console.log('pilot', pilot.value);
+    
+if (validateInput(pilot.value) === "Is a Number" || 
+    validateInput(copilot.value) === "Is a Number" || 
+    validateInput(fuelLevel.value) === "Not a Number" || 
+    validateInput(cargoLevel.value) === "Not a Number") {
+    alert("Invalid input");
+} else {
+    pilotStatus.innerHTML = `Pilot Name: ${pilot.value}`;
+    copilotStatus.innerHTML = `Co-pilot Name: ${copilot.value}`;
+    list.style.visibilty = 'visible !important';
+    console.log(list, list.style)
+}   if (fuelLevel < 10000) {
+    list.style.visibilty = 'visible';
 }
+};
 
 async function myFetch() {
     let planetsReturned;
