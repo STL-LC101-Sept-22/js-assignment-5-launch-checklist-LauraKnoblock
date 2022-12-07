ee8605cde5e12138fa45001b1599c1dbb872394d
LauraKnoblock <laura.e.knoblock@gmail.com>
2022-12-07 10:53:19 -0600

    finished and working, but does it pass the tests???

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 // Write your JavaScript code here!
 
+// const { pickPlanet } = require("./scriptHelper");
+
+// const { addDestinationInfo } = require("./scriptHelper");
+
+
+
+
+
+
+
 
 window.addEventListener("load", function() {
     console.log('page loaded');
@@ -12,6 +22,7 @@ window.addEventListener("load", function() {
         let copilotName = document.querySelector("input[name=copilotName]");
         let fuelLevel = document.querySelector("input[name=fuelLevel]");
         let cargoMass = document.querySelector("input[name=cargoMass]");
+       
         
         if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
             alert('All fields required!');
@@ -30,16 +41,26 @@ window.addEventListener("load", function() {
 
 
     });
-});
 
-   let listedPlanets;
+
+   let listedPlanets = [];
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
    let listedPlanetsResponse = myFetch();
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
    }).then(function () {
-       console.log(listedPlanets);
+
+        console.log(listedPlanets);
+       destination = pickPlanet(listedPlanets);
+    //    console.log(pickPlanet(listedPlanets));
+    //    console.log(missionTarget.innerHTML);
+    //    console.log(destination.name);
+    
+        pickPlanet(listedPlanets);
+        addDestinationInfo(document, destination.name, destination.diameter, destination.star, destination.distance, destination.moons, destination.image)
+        console.log(missionTarget.innerHTML);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
    });
    
+});
